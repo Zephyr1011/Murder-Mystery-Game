74de03ffef1e35606d48312efdd6ebdde951855a
Ebdavis29 <asa03192020@gmail.com>
2024-05-14 14:10:40 -0500

    changes

--- code-crew-clue/src/App.js
+++ code-crew-clue/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
-
+import {Instructions} from './Pages/Instructions'
+import { Suspects } from './Pages/Suspects';
+import{HashRouter,Routes,Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Home } from './Pages/Hpme';
 function App() {
   return (
+    
     <div className="App">
+      <HashRouter basename='/'>
       <header className="App-header">
         <img src="assets/CodeCrewClue.jpg.png" className="App-logo" alt="logo" />
       
@@ -17,12 +23,22 @@ function App() {
           </a>
           
           
-        <Button variant="primary" size='lg' style={{fontSize: "2rem"}}>
+        <Button variant="warning" size='lg' style={{fontSize: "2rem"}}>
           Start!
         </Button>{' '}
-       
+        <Link to="/Instructions">
+        <Button variant="warning" size='lg' style={{fontSize: "2rem"}}>
+          How To Play!
+        </Button>{' '}
+        </Link>
       </header>
      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Instructions' element={<Instructions />}/>
+        <Route path='/Suspects' element={<Suspects />}/>
+      </Routes>
+     </HashRouter>
     </div>
     
   );
---
+++ code-crew-clue/src/Pages/Instructions.js
@@ -0,0 +1,7 @@
+export function Instructions() {
+    return(
+        <h1>
+            This is the Instructions page!
+        </h1>
+    )
+}
