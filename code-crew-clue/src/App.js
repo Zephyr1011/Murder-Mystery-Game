d126afbeeeed9660db823138bfdd9551351f3cb2
Zephyr1011 <Quincymalone101@gmail.com>
2024-05-15 11:47:39 -0500

    Pages fixed

--- code-crew-clue/src/App.js
+++ code-crew-clue/src/App.js
@@ -11,31 +11,7 @@ function App() {
     
     <div className="App">
       <HashRouter basename='/'>
-      <header className="App-header">
-        <img src="assets/CodeCrewClue.jpg.png" className="App-logo" alt="logo" />
-      
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          </a>
-          
-          <div className='buttons'>
-            <Link to = '/Suspects'>
-        <Button variant="warning" size='lg' style={{fontSize: "2rem"}}>
-          Start!
-        </Button>{' '}
-        </Link>
-        <Link to="/Instructions">
-        <Button variant="warning" size='lg' style={{fontSize: "2rem"}} id='tutorialButton'>
-          How To Play!
-        </Button>{' '}
-        </Link>
-        </div>
-      </header>
-     
+
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Instructions' element={<Instructions />}/>
--- code-crew-clue/src/Pages/Hpme.js
+++ code-crew-clue/src/Pages/Hpme.js
@@ -1,5 +1,34 @@
+import { Button } from 'react-bootstrap';
+
+import { Link } from 'react-router-dom';
 export function Home() {
     return(
-        <h1> This is the Home page.</h1>
+        <>
+        <header className="App-header">
+        <img src="assets/CodeCrewClue.jpg.png" className="App-logo" alt="logo" />
+      
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          </a>
+          
+          <div className='buttons'>
+            <Link to = '/Suspects'>
+        <Button variant="warning" size='lg' style={{fontSize: "2rem"}}>
+          Start!
+        </Button>{' '}
+        </Link>
+        <Link to="/Instructions">
+        <Button variant="warning" size='lg' style={{fontSize: "2rem"}} id='tutorialButton'>
+          How To Play!
+        </Button>{' '}
+        </Link>
+        </div>
+      </header>
+      </>
+     
     )
 }
