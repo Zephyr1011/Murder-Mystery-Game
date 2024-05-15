3ab726cda76bc96d707a08e3b57910f3927fb683
Zephyr1011 <Quincymalone101@gmail.com>
2024-05-15 15:33:39 -0500

    backen commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,10 +7,12 @@ app.use(cors());
 const clues = [
     { id: 1, text: 'The butler' },
     { id: 2, text: 'The knife' },
-    { id: 3, text: 'In the library' }
+    { id: 3, text: 'In the library'},
+    { id: 4, text: 'Item Left Behind'}
 ];
 app.get('/api/clues', (req, res) => {
     res.json(clues);
+    res.send(clues);
 });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
