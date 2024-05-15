0904fffe56e8ebfce627b0dc1347266ac2c6d344
Zephyr1011 <Quincymalone101@gmail.com>
2024-05-15 09:58:10 -0500

    test

--- code-crew-clue/src/Pages/Instructions.js
+++ code-crew-clue/src/Pages/Instructions.js
@@ -1,7 +1,14 @@
 export function Instructions() {
     return(
+    <div>
         <h1>
-            This is the Instructions page!
+            How To Play!
         </h1>
+        <h4>
+            In this game you will have to figure out who commited a murder. 
+            There will be a list of six suspects for you to choose from and you will recive
+            clues throughout the course of the game. Can you find out who the murderer is?
+        </h4>
+    </div>
     )
 }
